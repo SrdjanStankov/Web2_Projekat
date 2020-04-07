8469aef4a779624aca4d844fd5f8e852096638fa
Srdjan Stankov <stankovsr@gmail.com>
2020-04-07 23:50:14 +0200

    Added backend login logic

--- PUSGS_Project/PUSGS_Project/ClientApp/src/app/services/backend.service.ts
+++ PUSGS_Project/PUSGS_Project/ClientApp/src/app/services/backend.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { User } from '../entities/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
+  registeredUsers: User[] = [];
+
+  private logedInUser: User = null;
+
   constructor() { }
+
+  login(email: string, password: string): [boolean, string] {
+    for (var i = 0; i < this.registeredUsers.length; i++) {
+      if (this.registeredUsers[i].email == email) {
+        if (this.registeredUsers[i].password == password) {
+          this.logedInUser = this.registeredUsers[i];
+          return [true, ''];
+        }
+        return [false, 'invalid password'];
+      }
+    }
+    return [false, 'invalid email'];
+  }
+
+  register(user: User): boolean {
+    for (var i = 0; i < this.registeredUsers.length; i++) {
+      if (this.registeredUsers[i].email == user.email) {
+        return false;
+      }
+    }
+    this.registeredUsers.push(user);
+    return true;
+  }
+
+  isLogedIn(): boolean {
+    return this.logedInUser != null;
+  }
 }
