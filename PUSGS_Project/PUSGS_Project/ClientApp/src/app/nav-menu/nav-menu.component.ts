af6f740746eaacc257a4b43c5bf701af64e198b2
Danilo Novakovic <dakenzi97@gmail.com>
2020-04-08 12:32:31 +0200

    User can now log-out after logging in. Added test user

--- PUSGS_Project/PUSGS_Project/ClientApp/src/app/nav-menu/nav-menu.component.ts
+++ PUSGS_Project/PUSGS_Project/ClientApp/src/app/nav-menu/nav-menu.component.ts
@@ -1,18 +1,18 @@
-import { Component } from '@angular/core';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from '../login/login.component';
-import { RegisterComponent } from '../register/register.component';
-import { BackendService } from '../services/backend.service';
+import { Component } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { LoginComponent } from "../login/login.component";
+import { RegisterComponent } from "../register/register.component";
+import { BackendService } from "../services/backend.service";
 
 @Component({
-  selector: 'app-nav-menu',
-  templateUrl: './nav-menu.component.html',
-  styleUrls: ['./nav-menu.component.css']
+  selector: "app-nav-menu",
+  templateUrl: "./nav-menu.component.html",
+  styleUrls: ["./nav-menu.component.css"],
 })
 export class NavMenuComponent {
   isExpanded = false;
 
-  constructor(private modalService: NgbModal, public backend: BackendService) { }
+  constructor(private modalService: NgbModal, public backend: BackendService) {}
 
   collapse() {
     this.isExpanded = false;
@@ -28,5 +28,8 @@ export class NavMenuComponent {
 
   openRegister() {
     this.modalService.open(RegisterComponent);
+  }
+  logout() {
+    this.backend.logout();
   }
 }
