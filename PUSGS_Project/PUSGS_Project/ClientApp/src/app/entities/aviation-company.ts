62b8580789ba687bc16d15653024cb96bc7844c8
Danilo Novakovic <dakenzi97@gmail.com>
2020-04-13 13:39:28 +0200

    Added aviation company and location classes

---
+++ PUSGS_Project/PUSGS_Project/ClientApp/src/app/entities/aviation-company.ts
@@ -0,0 +1,17 @@
+import { Location } from "./location";
+import { Flight } from "./flight";
+
+export class AviationCompany {
+  name: string;
+  address: Location;
+  description: string;
+  flights: Flight[];
+
+  /*
+  TODO: Dodati:
+   - destinacije na kojima posluje
+   - spisak karaka sa popustima za brzu rezervaciju
+   - konfiguraciju segmenata i mesta u avionima
+   - cenovnik i informacije o ptrljagu
+  */
+}
