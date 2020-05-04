c5dff857b26821361863364e3926e5cd9daf0255
Danilo Novakovic <dakenzi97@gmail.com>
2020-05-04 17:28:41 +0200

    Added additional fields to User

--- PUSGS_Project/Persistance/Migrations/20200504152749_AddedAdditionalPropertiesToUser.Designer.cs
+++ PUSGS_Project/Persistance/Migrations/20200504152749_AddedAdditionalPropertiesToUser.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistance;
 
 namespace Persistance.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200504152749_AddedAdditionalPropertiesToUser")]
+    partial class AddedAdditionalPropertiesToUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PUSGS_Project/Persistance/Migrations/20200504152749_AddedAdditionalPropertiesToUser.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.Migrations
+{
+    public partial class AddedAdditionalPropertiesToUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserEmail",
+                table: "User",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_UserEmail",
+                table: "User",
+                column: "UserEmail");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_User_User_UserEmail",
+                table: "User",
+                column: "UserEmail",
+                principalTable: "User",
+                principalColumn: "Email",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_User_User_UserEmail",
+                table: "User");
+
+            migrationBuilder.DropIndex(
+                name: "IX_User_UserEmail",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "UserEmail",
+                table: "User");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "User",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
