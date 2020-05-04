e10f58119f924a3d969314d9a0df4d2170186ddd
Danilo Novakovic <dakenzi97@gmail.com>
2020-05-04 16:54:56 +0200

    Added database related files

---
+++ PUSGS_Project/Persistance/ApplicationDbInitializer.cs
@@ -0,0 +1,18 @@
+﻿namespace Persistance
+{
+    public class ApplicationDbInitializer
+    {
+        public static void Initialize(ApplicationDbContext context)
+        {
+            var initializer = new ApplicationDbInitializer();
+            initializer.SeedEverything(context);
+        }
+
+        protected void SeedEverything(ApplicationDbContext context)
+        {
+            context.Database.EnsureCreated();
+
+            // TODO: Seed database
+        }
+    }
+}
