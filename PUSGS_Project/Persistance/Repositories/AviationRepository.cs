9685dec8834c331cc674dad6a537e2e591d8c33b
Danilo Novakovic <dakenzi97@gmail.com>
2020-05-25 15:41:27 +0200

    Implemented IAviationServices methods [HOT-FIX]

--- PUSGS_Project/Persistance/Repositories/AviationRepository.cs
+++ PUSGS_Project/Persistance/Repositories/AviationRepository.cs
@@ -1,8 +1,35 @@
-﻿using Core.Interfaces.Repositories;
+﻿using Core.Entities;
+using Core.Interfaces.Repositories;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Persistance.Repositories
 {
     public class AviationRepository : IAviationRepository
     {
+        public Task<long> AddAsync(AviationCompany company)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<IEnumerable<AviationCompany>> GetAllAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<AviationCompany> GetByIdAsync(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task RemoveAsync(long id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task UpdateAsync(AviationCompany company)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
