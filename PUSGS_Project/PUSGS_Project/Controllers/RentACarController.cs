b6b5a14954975dbd4e9fc42903a2639e2b112718
Srdjan Stankov <stankovsr@gmail.com>
2020-06-01 17:34:19 +0200

    Add Authorization on Rent a car controller

--- PUSGS_Project/PUSGS_Project/Controllers/RentACarController.cs
+++ PUSGS_Project/PUSGS_Project/Controllers/RentACarController.cs
@@ -14,15 +14,13 @@ using Microsoft.AspNetCore.Mvc;
 namespace PUSGS_Project.Controllers
 {
     [Route("api/[controller]")]
-    public class RentACarController : Controller
+    public class RentACarController : ApiController
     {
         private readonly IRentACarRepository repository;
-        private readonly IUserRepository userRepository;
 
-        public RentACarController(IRentACarRepository repository, IUserRepository userRepository)
+        public RentACarController(IRentACarRepository repository, IUserRepository userRepository) : base(userRepository)
         {
             this.repository = repository;
-            this.userRepository = userRepository;
         }
 
         // GET: api/<controller>
@@ -62,8 +60,7 @@ namespace PUSGS_Project.Controllers
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<object> Post([FromBody]RentACar model)
         {
-            var userEmail = User.Claims.FirstOrDefault(item => item.Type == AppConsts.CLAIM_TOKEN_KEY)?.Value;
-            var user = await userRepository.GetUserByEmailAsync(userEmail);
+            var user = await GetLoginUserAsync();
 
             if (user is SystemAdministrator)
             {
@@ -74,48 +71,77 @@ namespace PUSGS_Project.Controllers
                     Name = model.Name
                 };
 
-                // TODO: validate RentACar
-
                 if (!await repository.AddAsync(rentacar))
                 {
                     return BadRequest(new { message = "Already exist" });
                 }
                 return Ok();
             }
-            else
-            {
-                return Forbid();
-            }
+            
+            return Forbid();
         }
 
         [HttpPost]
         [Route("AddCar")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<object> AddCar([FromBody] CarModel model)
         {
-            await repository.AddCarToAgencyAsync(model.CarId, model.RentACarId);
-            return Ok();
+            var user = await GetLoginUserAsync();
+
+            if (user is RentACarAdministrator)
+            {
+                await repository.AddCarToAgencyAsync(model.CarId, model.RentACarId);
+                return Ok();
+            }
+
+            return Forbid();
         }
 
         [HttpPost]
         [Route("AddBranch")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<object> AddBranch([FromBody] BranchModel model)
         {
-            await repository.AddBranchToAgencyAsync(model.BranchId, model.RentACarId);
-            return Ok();
+            var user = await GetLoginUserAsync();
+            if (user is RentACarAdministrator)
+            {
+                await repository.AddBranchToAgencyAsync(model.BranchId, model.RentACarId);
+                return Ok();
+            }
+
+            return Forbid();
         }
 
         // PUT api/<controller>/5
         [HttpPut("{id}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<object> Put(int id, [FromBody]RentACar model)
         {
-            await repository.UpdateAsync(model);
-            return Ok();
+            var user = await GetLoginUserAsync();
+
+            if (user is RentACarAdministrator)
+            {
+                await repository.UpdateAsync(model);
+                return Ok();
+            }
+
+            return Forbid();
         }
 
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<object> Delete(int id)
         {
+            var user = await GetLoginUserAsync();
+
+            if (user is SystemAdministrator)
+            {
+                await repository.Delete(id);
+                return Ok();
+            }
+
+            return Forbid();
         }
     }
 }
