c581b5cd6b289bf57dddbbdabe1079038eca163c
Srdjan Stankov <stankovsr@gmail.com>
2020-06-02 18:22:29 +0200

    Add filter cars on home page

--- PUSGS_Project/PUSGS_Project/ClientApp/src/app/home/home.component.ts
+++ PUSGS_Project/PUSGS_Project/ClientApp/src/app/home/home.component.ts
@@ -5,6 +5,7 @@ import { BackendService } from '../services/backend.service';
 import { Router } from '@angular/router';
 import { AviationService } from '../services/aviation.service';
 import { AviationCompany } from '../entities/aviation-company';
+import { Car } from '../entities/car';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,18 @@ import { AviationCompany } from '../entities/aviation-company';
 export class HomeComponent {
   public aviationCompanies: AviationCompany[];
   public rentACarAgencies: RentACar[];
+  public carsToShow: Car[];
   public isCollapsedRAC: boolean;
   public isCollapsed: boolean;
+  public search: string = "";
+  public displayAgencies: boolean = true;
 
   constructor(private RACService: RentACarService, public backend: BackendService, private aviationService: AviationService, private router: Router) {
-    RACService.getAgencies().then(result => this.rentACarAgencies = result);
+    RACService.getAgencies().then(result => {
+      this.rentACarAgencies = result;
+    });
     aviationService.getAll().then(result => this.aviationCompanies = result);
+    this.displayAgencies = true;
   }
 
   details(id: number) {
@@ -26,5 +33,32 @@ export class HomeComponent {
   }
   aviationDetails(id: number) {
     this.router.navigateByUrl("aviation/" + id);
+  }
+
+  onFilterCar() {
+    const searchText = this.search.toLowerCase();
+    if (searchText.trim() == "") {
+      this.displayAgencies = true;
+    }
+    else {
+      this.displayAgencies = false;
+    }
+    this.carsToShow = [];
+    this.rentACarAgencies.filter(agency => {
+      agency.cars.filter(car => {
+        if (car.name.toLowerCase().includes(searchText)) {
+          this.carsToShow.push(car);
+        }
+        else if (car.type.toLowerCase().includes(searchText)) {
+          this.carsToShow.push(car);
+        }
+        else if (car.brand.toLowerCase().includes(searchText)) {
+          this.carsToShow.push(car);
+        }
+        else if (car.model.toLowerCase().includes(searchText)) {
+          this.carsToShow.push(car);
+        }
+      });
+    });
   }
 }
