80996992d4f7a17e16792e030f850d8fe493746b
Srdjan Stankov <stankovsr@gmail.com>
2020-08-07 22:24:10 +0200

    Added Discriminator to userdb

--- PUSGS_Project/Persistance.User/Configurations/UserConfiguration.cs
+++ PUSGS_Project/Persistance.User/Configurations/UserConfiguration.cs
@@ -1,10 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Persistance.User.Configurations
 {
     public class UserConfiguration : IEntityTypeConfiguration<Core.Entities.User>
     {
-        public void Configure(EntityTypeBuilder<Core.Entities.User> builder) => builder.HasKey(u => u.Email);
+        public void Configure(EntityTypeBuilder<Core.Entities.User> builder)
+        {
+            builder.HasKey(u => u.Email);
+            builder.HasDiscriminator<string>("Discriminator")
+                .HasValue<AviationAdministrator>(nameof(AviationAdministrator))
+                .HasValue<Core.Entities.User>(nameof(Core.Entities.User))
+                .HasValue<RentACarAdministrator>(nameof(RentACarAdministrator))
+                .HasValue<SystemAdministrator>(nameof(SystemAdministrator));
+        }
     }
 }
---
+++ PUSGS_Project/Persistance.User/Migrations/20200807201158_AddedDiscriminator.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistance.User.Migrations
+{
+    public partial class AddedDiscriminator : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "Users",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "Users");
+        }
+    }
+}
