57f0c4d0d3a31adcfabd023e298874790bef8865
Srdjan Stankov <stankovsr@gmail.com>
2020-08-17 18:57:50 +0200

    Added configuration support for running in container

--- PUSGS_Project/PUSGS_Project.ApiGateway/Startup.cs
+++ PUSGS_Project/PUSGS_Project.ApiGateway/Startup.cs
@@ -25,7 +25,19 @@ namespace PUSGS_Project.ApiGateway
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<Settings>(Configuration.GetSection("Settings"));
+            if (Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER") == "true")
+            {
+                services.Configure<Settings>(configOptions =>
+                    {
+                        configOptions.UserApiUrl = $"https://{Configuration["API_USER_SERVICE_SERVICE_HOST"]}:{Configuration["API_USER_SERVICE_SERVICE_PORT_HTTPS"]}";
+                        configOptions.RentACarApiUrl = $"https://{Configuration["API_RENTACAR_SERVICE_SERVICE_HOST"]}:{Configuration["API_RENTACAR_SERVICE_SERVICE_PORT_HTTPS"]}";
+                        configOptions.AviationAipUrl = $"https://{Configuration["API_AVIATION_SERVICE_SERVICE_HOST"]}:{Configuration["API_AVIATION_SERVICE_SERVICE_PORT_HTTPS"]}";
+                    }); 
+            }
+            else
+            {
+                services.Configure<Settings>(Configuration.GetSection("Settings"));
+            }
 
             services.AddCors();
 
